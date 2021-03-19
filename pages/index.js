80c12a4c6fa338733b655f0ac4878b6a9b984230
IvanSmiths <info@ivansmiths.com>
2021-03-19 21:09:45 +0100

    AAA

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,17 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
+      <main>
+        <section className="first">
+          <div>dddd</div>
+          <div>dddd</div>
+        </section>
+      </main>
+    </>
   );
 }
