d597eb97237ec8b285feb0e228ac027988f2055b
IvanSmiths <info@ivansmiths.com>
2021-03-20 16:13:16 +0100

    ADD Aurora effect

--- components/Hero.js
+++ components/Hero.js
@@ -12,14 +12,24 @@ const Hero = () => {
   return (
     <header>
       <section className="cnt-hero">
-        <div>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Perspiciatis, non.
+        <div className="hero-1">
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
+              molestias optio blanditiis hic odit velit veritatis nihil, officia
+              a dolorum?
+            </p>
+          </div>
+          <div className="hero-btn">
+            <h2>Contact</h2>
+            <h2>My works</h2>
+          </div>
         </div>
-        <div>
+        <div className="hero-2">
+          <span className="aurora aurora-1"></span>
           <Model />
         </div>
-        <div>
+        <div className="hero-3">
           <h1>
             IVAN <br /> SMITHS.
           </h1>
