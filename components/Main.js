93b995fd764630d142c11eb7aa9bd156aac44cbe
IvanSmiths <info@ivansmiths.com>
2021-03-20 16:50:25 +0100

    AAA

---
+++ components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      <h3>
+        Stuck with your PROJECT? <br /> I can help YOU
+      </h3>
+    </main>
+  );
+}
+
+export default Main;
