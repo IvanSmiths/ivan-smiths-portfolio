bf249886950c043fb3cb28990a68c6b30c5c9861
IvanSmiths <info@ivansmiths.com>
2021-05-02 21:00:09 +0200

    COMPLETE skills page

--- pages/skills.js
+++ pages/skills.js
@@ -14,27 +14,19 @@ const Skills = () => {
 
   return (
     <main className="main">
-      <h2 className="medium-font">MY 6 (+1) PACK</h2>
+      <h1 className="big-font">{t('skills:skills-desc')}</h1>
       <section className="card-cnt">
         <div className="card">
           <div className="card-content">
             <div className="card-front">
               <img
                 className="card-ico card-html"
-                src="./img/html-ico.svg"
+                src="/html-ico.svg"
                 alt="An icon of Html"
               />
             </div>
             <div className="card-back">
-              <p className="h6-title card-body">
-                <span className="paragraph-gray">&lt;p&gt;</span>
-                <strong className="highlight">
-                  SEO friendly code is a MUST!{' '}
-                </strong>
-                Search engine's web crawler are always pushing up well coded
-                websites, don't let your project drown!
-                <span className="paragraph-gray">&lt;/p&gt;</span>
-              </p>
+              <p className="small-font card-body">{t('skills:html')}</p>
             </div>
           </div>
         </div>
@@ -43,12 +35,12 @@ const Skills = () => {
             <div className="card-front">
               <img
                 className="card-ico"
-                src="./img/css-ico.svg"
+                src="/css-ico.svg"
                 alt="An icon of Css"
               />
             </div>
             <div className="card-back">
-              <p className="card-body h6-title"></p>
+              <p className="card-body small-font">{t('skills:css')}</p>
             </div>
           </div>
         </div>
@@ -57,12 +49,12 @@ const Skills = () => {
             <div className="card-front">
               <img
                 className="card-ico"
-                src="./img/javascript-ico.svg"
+                src="/javascript-ico.svg"
                 alt="An icon of Javascript"
               />
             </div>
             <div className="card-back">
-              <p className="card-body h6-title"></p>
+              <p className="card-body small-font">{t('skills:js')}</p>
             </div>
           </div>
         </div>
@@ -71,19 +63,12 @@ const Skills = () => {
             <div className="card-front">
               <img
                 className="card-ico card-react"
-                src="./img/react-ico.svg"
+                src="/react-ico.svg"
                 alt="An icon of React.js"
               />
             </div>
             <div className="card-back">
-              <p className="card-body h6-title">
-                <span className="paragraph-gray">&lt;&gt;</span>
-                React, with
-                <span className="highlight">Next.Js & Gatsby</span> is my most
-                valuable skill. I have great knowledge about making optimized
-                websites, both server side rendering, and static ones.
-                <span className="paragraph-gray">&lt;/&gt;</span>
-              </p>
+              <p className="card-body small-font">{t('skills:react')}</p>
             </div>
           </div>
         </div>
@@ -92,12 +77,12 @@ const Skills = () => {
             <div className="card-front">
               <img
                 className="card-ico card-node"
-                src="./img/nodejs-ico.svg"
+                src="/nodejs-ico.svg"
                 alt="An icon of Node.js"
               />
             </div>
             <div className="card-back">
-              <p className="card-body h6-title"></p>
+              <p className="card-body small-font">{t('skills:node')}</p>
             </div>
           </div>
         </div>
@@ -106,17 +91,12 @@ const Skills = () => {
             <div className="card-front">
               <img
                 className="card-ico card-mongodb"
-                src="./img/wordpress-ico.svg"
+                src="/wordpress-ico.svg"
                 alt="An icon of Wordpress"
               />
             </div>
             <div className="card-back">
-              <p className="card-body h6-title">
-                Yeah that's WordPress. Knowing how to choose the best plugins
-                and themes, is the key to have a
-                <span className="highlight">fast and optimized</span> WordPress
-                website.
-              </p>
+              <p className="card-body small-font">{t('skills:wp')}</p>
             </div>
           </div>
         </div>
@@ -125,18 +105,14 @@ const Skills = () => {
             <div className="card-front">
               <img
                 className="card-ico"
-                src="./img/blender-ico.svg"
+                src="/blender-ico.svg"
                 alt="An icon of Blender"
               />
             </div>
             <div className="card-back card-blender">
               <ModelCube />
 
-              <p className="card-body h6-title">
-                What's this?! This is Blender! A free and open source software
-                that i use, for making
-                <span className="highlight">3D/AR stuff</span>.
-              </p>
+              <p className="card-body small-font">{t('skills:blender')}</p>
             </div>
           </div>
         </div>
