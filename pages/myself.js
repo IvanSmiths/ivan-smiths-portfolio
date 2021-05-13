f2796f40762b5f3d88922d686d97a367c272f285
IvanSmiths <info@ivansmiths.com>
2021-05-13 10:35:15 +0200

    ADD Myself content

--- pages/myself.js
+++ pages/myself.js
@@ -14,66 +14,12 @@ const MySelf = () => {
         />
       </Head>
       <main className="myself-main">
-        <h1 className="big-font">
-          {t('myself:title')}{' '}
-          <strong className="highlight-main"> {t('myself:title2')}</strong>
-        </h1>
         <section className="myself-cnt">
           <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Distinctio sit quos veritatis vel corporis quasi, molestias quidem
-              quis delectus maiores et hic ullam earum nulla debitis culpa nemo?
-              Dolores neque facilis enim, quasi ratione accusantium assumenda,
-              illum error impedit magnam fugit vero maiores, nulla dolor
-              recusandae! Aliquam officia dolore facilis.
-            </p>
-            <div className="list-cnt">
-              <ul>
-                <li className="small-font">
-                  {t('myself:name')}{' '}
-                  <strong className="highlight2">{t('myself:name2')}</strong>
-                </li>
-                <li className="small-font">
-                  {t('myself:last')}{' '}
-                  <strong className="highlight2">{t('myself:last2')}</strong>
-                </li>
-                <li className="small-font">
-                  {t('myself:nationality')}{' '}
-                  <strong className="highlight2">
-                    {t('myself:nationality2')}
-                  </strong>
-                </li>
-                <li className="small-font">
-                  {t('myself:languages')}{' '}
-                  <strong className="highlight2">
-                    {t('myself:languages2')}
-                  </strong>
-                </li>
-              </ul>
-              <ul>
-                <li className="small-font">
-                  {t('myself:age')}{' '}
-                  <strong className="highlight2">{t('myself:age2')}</strong>
-                </li>
-                <li className="small-font">
-                  {t('myself:living')}{' '}
-                  <strong className="highlight2">{t('myself:living2')}</strong>
-                </li>
-                <li className="small-font">
-                  {t('myself:education')}{' '}
-                  <strong className="highlight2">
-                    {t('myself:education2')}
-                  </strong>
-                </li>
-                <li className="small-font">
-                  Email: {''}
-                  <strong className="highlight-main">
-                    info@ivansmiths.com
-                  </strong>
-                </li>
-              </ul>
-            </div>
+            <h1 className="big-font">
+              {t('myself:title')}{' '}
+              <strong className="highlight-main"> {t('myself:title2')}</strong>
+            </h1>
           </div>
           <div>
             <img
@@ -82,6 +28,44 @@ const MySelf = () => {
               src="myself-real.png"
               alt="A photo about me"
             />
+          </div>
+        </section>
+        <section className="myself-card-cnt">
+          <div className="pro-card">
+            <h2 className="small-font highlight">{t('myself:name')}</h2>
+            <h3 className="medium-font highlight-main">{t('myself:name2')}</h3>
+          </div>
+          <div className="pro-card">
+            <h2 className="small-font highlight"> {t('myself:last')}</h2>
+            <h3 className="medium-font highlight-main"> {t('myself:last2')}</h3>
+          </div>
+          <div className="pro-card">
+            <h2 className="small-font highlight"> {t('myself:education')}</h2>
+            <h3 className="medium-font highlight-main">
+              {t('myself:education2')}
+            </h3>
+          </div>
+          <div className="pro-card">
+            <h2 className="small-font highlight"> {t('myself:nationality')}</h2>
+            <h3 className="medium-font highlight-main">
+              {t('myself:nationality2')}
+            </h3>
+          </div>
+          <div className="pro-card">
+            <h2 className="small-font highlight"> {t('myself:languages')}</h2>
+            <h3 className="medium-font highlight-main">
+              {t('myself:languages2')}
+            </h3>
+          </div>
+          <div className="pro-card">
+            <h2 className="small-font highlight">{t('myself:age')}</h2>
+            <h3 className="medium-font highlight-main">{t('myself:age2')}</h3>
+          </div>
+          <div className="pro-card">
+            <h2 className="small-font highlight">{t('myself:living')}</h2>
+            <h3 className="medium-font highlight-main">
+              {t('myself:living2')}
+            </h3>
           </div>
         </section>
       </main>
