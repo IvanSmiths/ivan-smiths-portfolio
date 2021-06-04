7234e4f3dfe96663b228cbc6d81370682f45297d
IvanSmiths <info@ivansmiths.com>
2021-06-04 11:48:22 +0200

    ADD Google adsense

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script
+            data-ad-client="ca-pub-7914261934105452"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+          <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
